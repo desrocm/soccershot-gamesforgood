ce34d70d3bcfa757e884abe93777107017c8f62c
Michelle Desrochers <mdesrochers08@gmail.com>
2020-03-16 20:20:05 -0400

    first day dev
    - Environment is set, ball prefab created with bouce material, game controller code set up to intatiate the prefab ball on start only, get mouse to add force to ball
    
    - issue with ballforce not lifting the ball off the ground. Issue is either with gravity or with local direction being used in 'forward' vector3...

---
+++ SoccerShot/Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	[SerializeField]
+	public GameObject ballPrefab;
+
+	[SerializeField]
+	float ballForce;
+
+	GameObject ballInstance;
+	Vector3 mouseStart;
+	Vector3 mouseEnd;
+
+	float minDragDistance = 15f;
+	float zDepth = 25f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		CreateBall();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			mouseStart = Input.mousePosition;
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			mouseEnd = Input.mousePosition;
+
+			if(Vector3.Distance(mouseEnd,mouseStart) > minDragDistance)
+			{
+				//throw the ball if true. need rotation of ball for direction and add force 
+				Vector3 hitPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zDepth);
+				
+				//need to convert screen position to world position if yo want to use the mouse position to change something
+				hitPos = Camera.main.ScreenToWorldPoint(hitPos);
+
+				//rotate ball to hitPos.
+				ballInstance.transform.LookAt(hitPos);
+
+				//add relative force to make it local rb direction that was just changed
+				ballInstance.GetComponent<Rigidbody>().AddForce(Vector3.forward * ballForce, ForceMode.Impulse);
+			}
+
+		}
+
+
+	}
+
+	void CreateBall()
+	{
+		ballInstance = Instantiate(ballPrefab, ballPrefab.transform.position, Quaternion.identity) as GameObject;
+	}
+}
